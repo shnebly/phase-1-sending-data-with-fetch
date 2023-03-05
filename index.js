06b6d204b170682418cf57382057281f957e2baa
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-05 15:27:58 -0700

    Fetch data successfully POST to console and dbjson pages

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            userName: `${userName}`,
+            userEmail: `${userEmail}`,
+        })      
+    })
+    .then(resp => {
+        return resp.json();
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        alert("Try Again");
+        console.log(error.message);
+    })
+};
+
+submitData("Shelby", "snm@gmail.com");
+
+
+// const formData = {
+//     dogName: "Yoshi",
+//     dogBreed: "Aussie",
+// }
+// const configObj = {
+//     // method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// }
+// fetch("http://localhost:3000/dogs", configObj)
+//     .then(resp => {
+//         return resp.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         alert("Whoops");
+//         console.log(error.message);
+//     });
